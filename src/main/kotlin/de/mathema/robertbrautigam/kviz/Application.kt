819eed58d2a971736569b2b72f7ef25e65311245
Robert Braeutigam <robert@mathema.de>
2020-09-30 21:12:02 +0200

    Fix recursion to be safe

--- src/main/kotlin/de/mathema/robertbrautigam/kviz/Application.kt
+++ src/main/kotlin/de/mathema/robertbrautigam/kviz/Application.kt
@@ -1,15 +1,20 @@
 package de.mathema.robertbrautigam.kviz
 
+import arrow.core.Either.Companion.left
 import arrow.fx.IO
+import arrow.fx.extensions.fx
+import arrow.fx.typeclasses.Duration
 import java.util.*
+import java.util.concurrent.TimeUnit
 
-fun runApplication(currentObjects: CurrentObjects): IO<Unit> =
-    loopBody(currentObjects).flatMap(::runApplication)
+fun runApplication(currentObjects: CurrentObjects) =
+    IO.tailRecM(currentObjects, ::loopBody)
 
-private fun loopBody(currentObjects: CurrentObjects) =
-    IO { Thread.sleep(1000L) }.flatMap {
-        runBody(currentObjects)
-    }
+private fun loopBody(currentObjects: CurrentObjects) = IO.fx {
+    sleep(Duration(1000L, TimeUnit.MILLISECONDS)).bind()
+    val newObjects = runBody(currentObjects).bind()
+    left(newObjects)
+}
 
 private fun runBody(currentObjects: CurrentObjects) =
     objects().flatMap { objects ->
